710a7cf20a665b7463a9cda202d98ea893086215
Michael Osei <osei_michael@hotmail.com>
2025-01-23 13:51:56 +0000

    Add search functionality to product model

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,22 +1,32 @@
 class ServicesController < ApplicationController
+
   def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    if params[:query].present?
+      @services = Service.where('title ILIKE ? OR content ILIKE ?', "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      @services = Service.all
+    end
   end
 end
+
+#   def index
+#   end
+
+#   def show
+#   end
+
+#   def new
+#   end
+
+#   def create
+#   end
+
+#   def edit
+#   end
+
+#   def update
+#   end
+
+#   def destroy
+#   end
+# end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,3 +2,9 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :booking, dependant: :destroy
 end
+
+class Services < ApplicationRecord
+  def self.search(query)
+    where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+  end
+end
